58437f18b4d49e84380187fa0c829dc82cb544ef
Johan Haugland <johan@horde.no>
2022-04-19 14:04:26 +0200

    Initial commit of updated script
    - Old cmdlets (Azure.Rm)
    + New cmdlets (Az)
    + Subscription parameter
    + PrivateLink parameter
    + SQL server resource ID parameter
    + SQL & storage account credential name parameter
    - Plaintext password variables
    + Stored automation account credentials
    + Private Link parameter check, new flow
    + Automatic approval of new private endpoints
    + Storage account access key "credential"
    + Updated verbiage per MS guidelines

--- Backup-AzureDatabaseToBlobStorage.ps1
+++ Backup-AzureDatabaseToBlobStorage.ps1
@@ -1,24 +1,28 @@
 ﻿<#
 .SYNOPSIS
-	This Azure Automation runbook automates Azure SQL database backup to Blob storage and deletes old backups from blob storage. 
+	This Azure Automation runbook automates Azure SQL database backup to Blob storage and deletes old backups from blob storage.
 
 .DESCRIPTION
-	You should use this Runbook if you want manage Azure SQL database backups in Blob storage. 
-	This runbook can be used together with Azure SQL Point-In-Time-Restore.
+	You should use this Runbook if you want manage Azure SQL database backups in Blob storage.
+	This runbook can be used together with Azure SQL Point-In-Time-Restore. Now supports Private Link - endpoints.
 
 	This is a PowerShell runbook, as opposed to a PowerShell Workflow runbook.
+    This is an updated version of the original script found here: https://github.com/jlaari/PowerShell-Runbooks , https://github.com/azureautomation/backup-azure-sql-databases-to-blob-storage
+
+.PARAMETER SubscriptionId
+	Subscription ID of the Azure SQL server
 
 .PARAMETER ResourceGroupName
 	Specifies the name of the resource group where the Azure SQL Database server is located
+
+.PARAMETER saResourceId
+	Specifies the full resource ID for the storage account
 	
-.PARAMETER DatabaseServerName
+.PARAMETER sqlServerName
 	Specifies the name of the Azure SQL Database Server which script will backup
 	
-.PARAMETER DatabaseAdminUsername
-	Specifies the administrator username of the Azure SQL Database Server
-
-.PARAMETER DatabaseAdminPassword
-	Specifies the administrator password of the Azure SQL Database Server
+.PARAMETER aaCrSqlName
+	Name of the stored credential of an administrative user for the SQL server
 
 .PARAMETER DatabaseNames
 	Comma separated list of databases script will backup
@@ -29,8 +33,8 @@
 .PARAMETER BlobStorageEndpoint
 	Specifies the base URL of the storage account
 	
-.PARAMETER StorageKey
-	Specifies the storage key of the storage account
+.PARAMETER aaCrStorageKeyName
+	Name of the stored credential for the storage account access key
 
 .PARAMETER BlobContainerName
 	Specifies the container name of the storage account where backup file will be uploaded. Container will be created if it does not exist.
@@ -38,6 +42,9 @@
 .PARAMETER RetentionDays
 	Specifies the number of days how long backups are kept in blob storage. Script will remove all older files from container. 
 	For this reason dedicated container should be only used for this script.
+
+.PARAMETER PrivateLink
+    Boolean specifying use of Private Link connections to either resource.
 
 .INPUTS
 	None.
@@ -48,29 +55,36 @@
 #>
 
 param(
-    [parameter(Mandatory=$true)]
-	[String] $ResourceGroupName,
-    [parameter(Mandatory=$true)]
-	[String] $DatabaseServerName,
-    [parameter(Mandatory=$true)]
-    [String]$DatabaseAdminUsername,
 	[parameter(Mandatory=$true)]
-    [String]$DatabaseAdminPassword,
+	[string]$SubscriptionId,
+    [parameter(Mandatory=$true)]
+	[string]$ResourceGroupName,
+    [parameter(Mandatory=$true)]
+	[string]$saResourceId,
+    [parameter(Mandatory=$true)]
+	[string]$sqlServerName,
+    [parameter(Mandatory=$true)]
+	[string]$aaCrSqlName,
 	[parameter(Mandatory=$true)]
-    [String]$DatabaseNames,
+    [string]$DatabaseNames,
     [parameter(Mandatory=$true)]
-    [String]$StorageAccountName,
+    [string]$StorageAccountName,
     [parameter(Mandatory=$true)]
-    [String]$BlobStorageEndpoint,
+    [string]$BlobStorageEndpoint,
     [parameter(Mandatory=$true)]
-    [String]$StorageKey,
+    [string]$aaCrStorageKeyName,
 	[parameter(Mandatory=$true)]
     [string]$BlobContainerName,
 	[parameter(Mandatory=$true)]
-    [Int32]$RetentionDays
+    [Int32]$RetentionDays,
+    [parameter(Mandatory=$true)]
+    [Boolean]$PrivateLink
 )
 
 $ErrorActionPreference = 'stop'
+
+# Requirement to retrieve PSCredential objects
+Import-Module Orchestrator.AssetManagement.Cmdlets -ErrorAction SilentlyContinue
 
 function Login() {
 	$connectionName = "AzureRunAsConnection"
@@ -80,11 +94,13 @@ function Login() {
 
 		Write-Verbose "Logging in to Azure..." -Verbose
 
-		Add-AzureRmAccount `
+		Add-AzAccount `
 			-ServicePrincipal `
 			-TenantId $servicePrincipalConnection.TenantId `
 			-ApplicationId $servicePrincipalConnection.ApplicationId `
 			-CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint | Out-Null
+
+		Set-AzContext -SubscriptionId $SubscriptionId
 	}
 	catch {
 		if (!$servicePrincipalConnection)
@@ -98,70 +114,102 @@ function Login() {
 	}
 }
 
-function Create-Blob-Container([string]$blobContainerName, $storageContext) {
+function New-Blob-Container([string]$blobContainerName, $storageContext) {
 	Write-Verbose "Checking if blob container '$blobContainerName' already exists" -Verbose
-	if (Get-AzureStorageContainer -ErrorAction "Stop" -Context $storageContext | Where-Object { $_.Name -eq $blobContainerName }) {
+	if (Get-AzStorageContainer -ErrorAction "Stop" -Context $storageContext | Where-Object { $_.Name -eq $blobContainerName }) {
 		Write-Verbose "Container '$blobContainerName' already exists" -Verbose
 	} else {
-		New-AzureStorageContainer -ErrorAction "Stop" -Name $blobContainerName -Permission Off -Context $storageContext
+		New-AzStorageContainer -ErrorAction "Stop" -Name $blobContainerName -Permission Off -Context $storageContext
 		Write-Verbose "Container '$blobContainerName' created" -Verbose
 	}
 }
 
-function Export-To-Blob-Storage([string]$resourceGroupName, [string]$databaseServerName, [string]$databaseAdminUsername, [string]$databaseAdminPassword, [string[]]$databaseNames, [string]$storageKey, [string]$blobStorageEndpoint, [string]$blobContainerName) {
-	Write-Verbose "Starting database export to databases '$databaseNames'" -Verbose
-	$securePassword = ConvertTo-SecureString –String $databaseAdminPassword –AsPlainText -Force 
-	$creds = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $databaseAdminUsername, $securePassword
+function Export-To-Blob-Storage([string]$ResourceGroupName, [string]$sqlServerName, [string]$aaCrSqlName, [string[]]$databaseNames, [string]$blobStorageEndpoint, [string]$blobContainerName, [string]$saResourceId, [Boolean]$PrivateLink) {
+	Write-Verbose "Starting database export of databases '$databaseNames'" -Verbose
+	$sqlCred = Get-AutomationPSCredential -Name $aaCrSqlName
 
 	foreach ($databaseName in $databaseNames.Split(",").Trim()) {
-		Write-Output "Creating request to backup database '$databaseName'"
-
+		
 		$bacpacFilename = $databaseName + (Get-Date).ToString("yyyyMMddHHmm") + ".bacpac"
 		$bacpacUri = $blobStorageEndpoint + $blobContainerName + "/" + $bacpacFilename
 
-		$exportRequest = New-AzureRmSqlDatabaseExport -ResourceGroupName $resourceGroupName –ServerName $databaseServerName `
-			–DatabaseName $databaseName –StorageKeytype "StorageAccessKey" –storageKey $storageKey -StorageUri $BacpacUri `
-			–AdministratorLogin $creds.UserName –AdministratorLoginPassword $creds.Password -ErrorAction "Stop"
-		
-		# Print status of the export
-		Get-AzureRmSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink -ErrorAction "Stop"
+		if ($PrivateLink) {
+			Write-Verbose "Getting SQL server resource details"
+			$sqlServer = Get-AzSqlServer -ResourceGroupName $ResourceGroupName -ServerName $sqlServerName
+			
+			Write-Verbose "Creating request to backup database '$databaseName' (over Private Link)"
+			$exportRequest = New-AzSqlDatabaseExport `
+				-ResourceGroupName $ResourceGroupName `
+				-ServerName $sqlServerName `
+				-DatabaseName $databaseName `
+				-StorageKeytype "StorageAccessKey" `
+				-storageKey $storageKey.GetNetworkCredential().Password `
+				-StorageUri $BacpacUri `
+				-AdministratorLogin $sqlCred.UserName `
+				-AdministratorLoginPassword $sqlCred.Password `
+				-UseNetworkIsolation $PrivateLink `
+				-StorageAccountResourceIdForPrivateLink $saResourceId `
+				-SqlServerResourceIdForPrivateLink $sqlServer.ResourceId `
+				-ErrorAction "Stop"
+
+			Start-Sleep -Seconds 30
+
+			## Approve the Private Endpoint Connection for the Storage Account
+			$saPrivateEndpoint = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $saResourceId | `
+				Where-Object {($_.PrivateEndpoint.Id -like "*ImportExportPrivateLink_Storage*" -and $_.PrivateLinkServiceConnectionState.status -eq "Pending")}
+			Approve-AzPrivateEndpointConnection -ResourceId $saPrivateEndpoint.Id
+
+			## Approve the Private Endpoint Connection for the SQL Account
+			$sqlPrivateEndpoint = Get-AzPrivateEndpointConnection -PrivateLinkResourceId $sqlServer.ResourceId | `
+				Where-Object {($_.PrivateEndpoint.Id -like "*ImportExportPrivateLink_SQL*" -and $_.PrivateLinkServiceConnectionState.status -eq "Pending")}
+			Approve-AzPrivateEndpointConnection -ResourceId $sqlPrivateEndpoint.Id
+
+			Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink | Where-Object {($_.Status -eq "InProgress")}
+		} else {
+			Write-Verbose "Creating request to backup database '$databaseName'"
+			$exportRequest = New-AzSqlDatabaseExport -ResourceGroupName $ResourceGroupName -ServerName $sqlServerName `
+			-DatabaseName $databaseName -StorageKeytype "StorageAccessKey" -storageKey $storageKey.GetNetworkCredential().Password -StorageUri $BacpacUri `
+			-AdministratorLogin $sqlCred.UserName -AdministratorLoginPassword $sqlCred.Password -ErrorAction "Stop"
+
+			Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $exportRequest.OperationStatusLink -ErrorAction "Stop"
+		}
 	}
 }
 
-function Delete-Old-Backups([int]$retentionDays, [string]$blobContainerName, $storageContext) {
+function Remove-Old-Backups([int]$retentionDays, [string]$blobContainerName, $storageContext) {
 	Write-Output "Removing backups older than '$retentionDays' days from blob: '$blobContainerName'"
 	$isOldDate = [DateTime]::UtcNow.AddDays(-$retentionDays)
-	$blobs = Get-AzureStorageBlob -Container $blobContainerName -Context $storageContext
+	$blobs = Get-AzStorageBlob -Container $blobContainerName -Context $storageContext
 	foreach ($blob in ($blobs | Where-Object { $_.LastModified.UtcDateTime -lt $isOldDate -and $_.BlobType -eq "BlockBlob" })) {
 		Write-Verbose ("Removing blob: " + $blob.Name) -Verbose
-		Remove-AzureStorageBlob -Blob $blob.Name -Container $blobContainerName -Context $storageContext
+		Remove-AzStorageBlob $blob.Name -Container $blobContainerName -Context $storageContext
 	}
 }
 
 Write-Verbose "Starting database backup" -Verbose
 
-$StorageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey
+$storageKey = Get-AutomationPSCredential -Name $aaCrStorageKeyName
+$StorageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey.GetNetworkCredential().Password
 
 Login
 
-Create-Blob-Container `
+New-Blob-Container `
 	-blobContainerName $blobContainerName `
 	-storageContext $storageContext
 	
 Export-To-Blob-Storage `
 	-resourceGroupName $ResourceGroupName `
-	-databaseServerName $DatabaseServerName `
-	-databaseAdminUsername $DatabaseAdminUsername `
-	-databaseAdminPassword $DatabaseAdminPassword `
+	-sqlServerName $sqlServerName `
+	-aaCrSqlName $aaCrSqlName `
 	-databaseNames $DatabaseNames `
-	-storageKey $StorageKey `
 	-blobStorageEndpoint $BlobStorageEndpoint `
-	-blobContainerName $BlobContainerName
+	-blobContainerName $BlobContainerName `
+	-saResourceId $saResourceId `
+	-PrivateLink $PrivateLink
 	
-Delete-Old-Backups `
+Remove-Old-Backups `
 	-retentionDays $RetentionDays `
 	-storageContext $StorageContext `
 	-blobContainerName $BlobContainerName
 	
 Write-Verbose "Database backup script finished" -Verbose
-
--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 Useful Azure Automation Runbooks to maintain Azure services
 
-* Start & Shutdown Azure Cloud Services
 * Backup SQL Database to blob storage (to support offsite backups)
-* Enable & disable Application Insights web tests
-* Enable & disable Application Insights alert rules
+
+     Supports connections over Private Link with recently added functionality (https://techcommunity.microsoft.com/t5/azure-sql-blog/import-export-using-private-link-now-in-preview/ba-p/3256192)
 
 If running in Azure automation account
 * Update Powershell modules to latest version before running scripts
@@ -13,7 +12,6 @@ See more information about Azure Automation: https://docs.microsoft.com/en-us/az
 
 If running locally
 * Remove login part from the script
-* Run Login-AzureRmAccount to login you Azure subscription if script is using AzureRunAsConnection 
-* Run Add-AzureAccount to login you Azure subscription if script is using AzureClassicRunAsConnection
+* Run Login-AzAccount or Connect-AzAccount to login you Azure subscription if script is using AzureRunAsConnection
 
 You need Azure Powershell cmdlets when running scripts locally. See more information from https://docs.microsoft.com/en-us/powershell/azureps-cmdlets-docs/
